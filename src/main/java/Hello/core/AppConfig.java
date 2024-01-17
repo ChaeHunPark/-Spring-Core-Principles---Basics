77407465d77528177ae007b2a4246aca49927d98
cogns <cogns8571@naver.com>
2024-01-17 14:14:48 +0900

    add Spring Container

--- src/main/java/Hello/core/AppConfig.java
+++ src/main/java/Hello/core/AppConfig.java
@@ -2,37 +2,39 @@ package Hello.core;
 
 import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
+import Hello.core.discount.RateDiscountPolicy;
 import Hello.core.member.MemberRepository;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServiceImpl;
 import Hello.core.member.MemoryMemberRepository;
 import Hello.core.order.OrderService;
 import Hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
-// 생성자한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다. (injection)
-// 의존성 주입(Dependency Injection)
+@Configuration // App의 구성(설정)정보를 담당.
 public class AppConfig {
 
-    //역할
+    @Bean // Spring Container에 등록
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    //구현
-    //메서드명이 드러나도록 리팩토링, Service에서 사용할 객체를 바꾸려면 이 코드를 고치면된다.
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    //역할
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
 
-    //구현
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new DiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/Hello/core/MemberApp.java
+++ src/main/java/Hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import Hello.core.member.Grade;
 import Hello.core.member.Member;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // 컨테이너 사용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//Bean 객체를 찾는다.
+
         Member member = new Member(1L, "mamberA", Grade.VIP);
         memberService.join(member);
 
